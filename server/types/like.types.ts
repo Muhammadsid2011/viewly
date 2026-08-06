ce103379410edcbe85245233597a5b7f4d7c5622
Muhammadsid2011 <muhammadsid2011@gmail.com>
2026-08-06 13:54:28 +0500

    Make video, comment, and tweet fields optional in ILike interface

--- server/types/like.types.ts
+++ server/types/like.types.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export interface ILike {
     _id: mongoose.Types.ObjectId;
-    video: mongoose.Types.ObjectId;
-    comment: mongoose.Types.ObjectId
-    tweet: mongoose.Types.ObjectId;
+    video?: mongoose.Types.ObjectId;
+    comment?: mongoose.Types.ObjectId
+    tweet?: mongoose.Types.ObjectId;
     likedBy: mongoose.Types.ObjectId;
     createdAt: Date;
     updatedAt: Date;
